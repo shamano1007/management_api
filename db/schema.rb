091dcc17fa19313b939510c59824e0f2afaa74af
s-hamano <s.hamano1007@gmail.com>
2019-05-26 21:24:33 +0900

    users カラム修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2019_05_26_114145) do
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "email"
     t.text "tokens"
